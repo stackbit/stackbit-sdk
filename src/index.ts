bfd062f2fc7012efcfb553340324c4c902f87d02
Ramon Snir <r@mon.dev>
2021-03-03 17:48:19 -0500

    export ContentItem

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from './config/config-schema';
 export { ObjectModel, DataModel, ConfigModel, PageModel, Model, Config } from './config/config-loader';
 export * as ConfigLoader from './config/config-loader';
+export { ContentItem } from './content/content-loader';
 export * as ContentLoader from './content/content-loader';
