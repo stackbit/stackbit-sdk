b3d41495ddceea7b1b8400ea704efd8f1b1632e1
Simon Hanukaev <semenh@gmail.com>
2021-03-08 20:14:32 +0200

    better cli
    - quiet mode
    - colors
    - global validation message

--- src/cli/cli-utils.ts
+++ src/cli/cli-utils.ts
@@ -1,77 +1,124 @@
 import _ from 'lodash';
+import chalk from 'chalk';
 import { Config, loadConfig } from '../config/config-loader';
 import { loadContent } from '../content/content-loader';
 import { ContentValidationError } from '../content/content-errors';
 
-export async function validate({ inputDir, configOnly }: { inputDir: string; configOnly: boolean }) {
-    console.log(`validating stackbit configuration at ${inputDir}`);
-    const result = await loadConfig({
+const red = chalk.red;
+const green = chalk.green;
+const redCross = red('✘');
+
+interface ValidateOptions {
+    inputDir: string;
+    configOnly: boolean;
+    quiet: boolean;
+}
+
+export async function validate({ inputDir, configOnly, quiet }: ValidateOptions) {
+    const quietConsole = getQuietConsole({ quiet });
+    quietConsole.debug(`loading and validating Stackbit configuration from: ${inputDir}`);
+
+    const configResult = await loadConfig({
         dirPath: inputDir
     });
-    if (result.errors.length === 0) {
-        console.log('configuration is valid');
+
+    quietConsole.group();
+    if (configResult.errors.length === 0) {
+        console.log(green('✔ configuration is valid'));
     } else {
-        console.group(`found ${result.errors.length} errors in configuration:`);
-        result.errors.forEach((error) => {
-            console.log(error.message);
+        console.group(red(`found ${configResult.errors.length} errors in configuration:`));
+        configResult.errors.forEach((error) => {
+            console.log(`${redCross} ${error.message}`);
         });
         console.groupEnd();
     }
-    if (!configOnly && result.config) {
-        await validateContent({
-            dirPath: inputDir,
-            config: result.config,
-            skipUnmodeledContent: false
-        });
+    quietConsole.groupEnd();
+
+    if (configOnly || !configResult.config) {
+        return;
+    }
+
+    const contentResult = await validateContent({
+        dirPath: inputDir,
+        config: configResult.config,
+        quiet: quiet,
+        skipUnmodeledContent: false
+    });
+
+    if (configResult.errors.length === 0 && contentResult.errors.length === 0) {
+        console.log(green('✔ validation passed'));
+    } else {
+        console.log(red('✘ validation failed, see errors above'));
     }
 }
 
 interface ValidateContentOptions {
     dirPath: string;
     config: Config;
+    quiet: boolean;
     skipUnmodeledContent: boolean;
 }
 
-export async function validateContent({ dirPath, config, skipUnmodeledContent }: ValidateContentOptions) {
-    console.log(`loading content from ${dirPath}`);
+export async function validateContent({ dirPath, config, quiet, skipUnmodeledContent }: ValidateContentOptions) {
+    const quietConsole = getQuietConsole({ quiet });
+    quietConsole.group(`loading and validating content from: ${dirPath}`);
+
     const result = await loadContent({ dirPath, config, skipUnmodeledContent });
-    console.group(`loaded ${result.contentItems.length} files`);
     const { modeledItems, unmodeledItems } = _.groupBy(result.contentItems, (contentItem) =>
         contentItem.__metadata.modelName !== null ? 'modeledItems' : 'unmodeledItems'
     );
-    if (typeof modeledItems !== 'undefined' && modeledItems.length > 0) {
-        const modeledItemsByModelName = _.groupBy(modeledItems, (contentItem) => {
-            return contentItem.__metadata.modelName;
-        });
-        console.group(`${modeledItems.length} files matched to models:`)
+    quietConsole.debug(
+        `loaded ${result.contentItems.length} files in total (${(modeledItems || []).length} matched, ${(unmodeledItems || []).length} unmatched)`
+    );
+
+    if (!quiet && typeof modeledItems !== 'undefined' && modeledItems.length > 0) {
+        const modeledItemsByModelName = _.groupBy(modeledItems, (contentItem) => contentItem.__metadata.modelName);
+        console.group(`${modeledItems.length} files matched to models:`);
         _.forEach(modeledItemsByModelName, (contentItems, modelName) => {
             console.group(`${modelName}: ${contentItems.length} files:`);
-            _.forEach(contentItems, contentItem => {
+            _.forEach(contentItems, (contentItem) => {
                 console.log(contentItem.__metadata.filePath);
             });
             console.groupEnd();
         });
+        console.groupEnd();
     }
-    if (typeof unmodeledItems !== 'undefined' && unmodeledItems.length > 0) {
-        console.group(`${unmodeledItems.length} files could not be matched to models:`)
-        _.forEach(unmodeledItems, contentItem => {
+
+    if (!quiet && typeof unmodeledItems !== 'undefined' && unmodeledItems.length > 0) {
+        console.group(red(`${unmodeledItems.length} files could not be matched to models:`));
+        _.forEach(unmodeledItems, (contentItem) => {
             console.log(contentItem.__metadata.filePath);
         });
         console.groupEnd();
     }
-    console.groupEnd();
+
     if (result.errors.length === 0) {
-        console.log('content is valid');
+        console.log(green('✔ content files are valid'));
     } else {
-        console.group(`found ${result.errors.length} errors in content:`);
+        console.group(red(`found ${result.errors.length} errors in content files:`));
         result.errors.forEach((error) => {
             if (error instanceof ContentValidationError) {
-                console.log(`${error.filePath} (${error.modelName}): ${error.message}`);
+                console.log(`${redCross} ${error.filePath} (${error.modelName}): ${error.message}`);
             } else {
-                console.log(error.message);
+                console.log(`${redCross} ${error.message}`);
             }
         });
         console.groupEnd();
     }
+
+    quietConsole.groupEnd();
     return result;
+}
+
+function getQuietConsole({ quiet }: { quiet: boolean }) {
+    if (!quiet) {
+        return console;
+    }
+    const noop = () => {};
+    const quietConsole = new console.Console({ stdout: process.stdout, stderr: process.stderr });
+    quietConsole.debug = noop;
+    quietConsole.error = noop;
+    quietConsole.group = noop;
+    quietConsole.groupEnd = noop;
+    return quietConsole;
 }
