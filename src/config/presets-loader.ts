5f37e62648db23f6584023293654d415b6599c40
davbree <david@stackbit.com>
2022-01-02 11:34:44 +0200

    Use _.set

--- src/config/presets-loader.ts
+++ src/config/presets-loader.ts
@@ -45,7 +45,7 @@ export async function loadPresets(dirPath: string, config: Config): Promise<Pres
             if (preset.thumbnail) {
                 preset.thumbnail = resolveThumbnailPath(preset.thumbnail, presetsRelDir);
             }
-            preset.modelName = presetData.model;
+            _.set(preset, 'modelName', presetData.model);
             append(presetsByModel, presetData.model, presetId);
         });
     }
